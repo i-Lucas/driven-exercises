d1e4206f0b8782d9b632439176cc5cc1703a4430
Lucas Oliveira <93502599+i-Lucas@users.noreply.github.com>
2022-04-19 23:37:24 -0300

    feat: implement solution for primeiro-servidor exercise

--- primeiro-servidor/index.js
+++ primeiro-servidor/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
-// import cors from 'cors';
+import cors from 'cors';
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 app.get('/hello', (req, res) => {
 
